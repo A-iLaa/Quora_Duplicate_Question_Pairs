859b3f56148da9bc5a1c106073bf5122d9c52fc9
Akhila <137712436+A-iLaa@users.noreply.github.com>
2024-07-14 12:40:03 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -5,7 +5,7 @@ from fuzzywuzzy import fuzz
 import numpy as np
 import joblib
 import gensim
-import nltk.corpus import stopwords
+from nltk.corpus import stopwords
 
 cv = joblib.load('cv.joblib')
 
